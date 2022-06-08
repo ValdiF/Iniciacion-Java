894de0191521df2541b3c5599e1c7af83230aef5
David Valverde <93525363+ValdiF@users.noreply.github.com>
2022-06-08 18:02:47 +0200

    Create Main.java

---
+++ herencia/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+        
+    Hija hija= new Hija();
+    
+    System.out.println(hija.saludar());
+        
+    hija.Hija("salu",2); 
+    hija.prueba();
+    }
+
+
+    
+}
